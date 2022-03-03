7b381f45a6699779cc3ed17a91216ade0f8a123a
Eduardo Roseo <eduardoroseo@gmail.com>
2022-03-03 13:38:14 -0300

    hotfix: removendo warnings de header já declarado

---
+++ resolve-warning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# list all php files in the ./teampass/sources directory and comment the line containing "header('Content-type: text/html; charset=utf-8');
+
+
+for file in ./teampass/sources/*.php
+do
+    echo "Processing $file"
+    sed -i -e 's/header('\''Content-type: text\/html; charset=utf-8'\'');/\/\/header('\''Content-type: text\/html; charset=utf-8'\'');/g' "$file"
+done
+
+for file in ./teampass/sources/*.php
+do
+    echo "Processing $file"
+    sed -i -e 's/header('\''Cache-Control: no-cache, must-revalidate'\'');/\/\/header('\''Cache-Control: no-cache, must-revalidate'\'');/g' "$file"
+done
